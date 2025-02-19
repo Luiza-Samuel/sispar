ad0a9a2c9a69fa63197c4ce66ffe0f5c6d656249
Luiza-Samuel <luizaandradedeoliveira@gmail.com>
2025-02-19 18:55:27 -0300

    Primeiro commit

---
+++ src/components/Solicitacao/Solicitacao.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../navbar/NavBar.jsx";
+import styles from "./Solicitacao.module.scss";
+import Home from "../../assets/Dashboard/home header.png";
+import Seta from "../../assets/Dashboard/Vector.png";
+
+function Solicitacao(){
+    return(
+        <div className = {styles.mainsolicitacao}>
+        <form className={styles.fomsolicitacao}></form>
+        </div>
+    ) 
+}
+export default Solicitacao
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom"
+//import Capa from "../../assets/Tela Login/imagem tela de login.png";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss"
+
+function Login() {
+
+  const navigate = useNavigate() //Iniciando o hook useNavigate
+
+  const irParaReembolsos = () => {
+    navigate("/reembolsos")  //Redirecionando para a página de reembolsos
+  }
+
+  return (
+    <main>
+      <section className={styles.containerFoto}>
+        {/* <img src={Capa} alt="Foto de um navio cargueiro" /> */}
+      </section>
+
+      <section className={styles.formWapper}>
+        <div className={styles.boxLogo}>
+          <img src={Logo} alt="Logo da wilson sons" />
+          <h1>Boas vindas ao Novo Portal SISPAR </h1>
+          <p>Sistema de Emissão de Boletos e Parcelamento</p>
+        </div>
+
+        <form action="">
+          <input type="email" name="email" id="email" placeholder="Email" />
+
+          <input type="password" name="senha" id="Senha" placeholder="Senha" />
+
+          <a href="">Esqueci minha senha</a>
+          
+          <div>
+           <button onClick={irParaReembolsos} className={styles.buttonEntrar}>Entrar</button>
+            <button className={styles.buttonCriar}>Criar conta</button>
+          </div>
+
+        </form>
+      </section>
+    </main>
+  );
+}
+export default Login;
